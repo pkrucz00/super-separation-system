90a500a608028b9785218bb3880b0d062d4180cb
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2022-11-13 13:40:58 +0100

    Revert added (sounds like � but it's the best I can do)

--- sss/extractors/nussl.py
+++ sss/extractors/nussl.py
@@ -2,9 +2,11 @@ import nussl
 import numpy as np
 import librosa
 
-from sss.dataclasses import ExtractParams, ResultWaves, Instrument
+from sss.dataclasses import ExtractParams, ResultWaves, Instrument, AudioWave
 from pathlib import Path
 import pickle
+
+from functools import reduce
 
 
 def perform_nussl(extract_params: ExtractParams) -> ResultWaves:
@@ -15,17 +17,23 @@ def perform_nussl(extract_params: ExtractParams) -> ResultWaves:
             NMF, _, _ = pickle.load(file)
             return NMF
     
-    def compute_audio_wave(W, H):
+    def compute_audio_wave(W, H) -> AudioWave:
         compute_one_channel = lambda n: librosa.istft(W.T @ H[:, :, n], n_fft=2048, hop_length=1024)  #TODO debug weird sound artifacts and double length
-        return np.array([1000*compute_one_channel(0), 1000*compute_one_channel(1)], dtype=np.float64).T
+        result_array = np.array([compute_one_channel(0), compute_one_channel(1)], dtype=np.float64)
+        return nussl.AudioSignal(audio_data_array=result_array).peak_normalize().apply_gain(2).audio_data.T
     
-    def compute_reversed_wave(original: nussl.AudioSignal, result_waves):
-        return np.array([[],[]])   #TODO add reversal
+    def results_to_signals(results: ResultWaves, duration: float) -> list[nussl.AudioSignal]:
+        return [nussl.AudioSignal(audio_data_array=wave).truncate_seconds(duration).peak_normalize() for _instr, wave in results]
+        
+    def compute_reversed_wave(original: nussl.AudioSignal, result_signals: list[nussl.AudioSignal]) -> AudioWave:
+        original.peak_normalize()
+        subtraction = reduce(lambda as1, as2: as1 - as2, result_signals, original)
+        return subtraction.audio_data.T   #TODO add reversal
     
     sig = nussl.AudioSignal(extract_params.input_path)
     models = [load_model(instr) for instr in extract_params.instruments]
     matrices = [nussl.separation.NMFMixin.transform(sig, model) for model in models]
     results = [ (instr, compute_audio_wave(W, H)) for instr, (W, H) in zip(extract_params.instruments, matrices)]
-    # if extract_params.reverse:
-    #     results.append({Instrument("other"): compute_reversed_wave()})
+    if extract_params.reverse:
+        results.append((Instrument("other"), compute_reversed_wave(sig, results_to_signals(results, sig.signal_duration))))
     return results

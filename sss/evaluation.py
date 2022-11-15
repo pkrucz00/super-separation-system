d883c2d8ee8d473af4d49ae32246844203f8aeea
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2022-11-15 16:12:51 +0100

    Applying changes

--- sss/evaluation.py
+++ sss/evaluation.py
@@ -4,11 +4,11 @@ import soundfile as sf
 from sss.dataclasses import EvalParams, AudioWave
 
 
-def evaluate_results(result_wave: AudioWave, eval_params: EvalParams) -> dict:
-    def evaluate_single(soutput_wave, reference):
+def evaluate_results(result_wave: AudioWave, eval_params: EvalParams) -> tuple:
+    def evaluate_single(reference):
         results = museval.evaluate([reference], [result_wave])
         return tuple([res.flatten() for res in results])
     
-    (f"Starting result evaluation...")
+    print(f"Starting result evaluation...")
     reference, _ = sf.read(eval_params.ref_path)
-    return evaluate_single(result_wave, reference)
+    return evaluate_single(reference)
